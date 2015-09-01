07ef8d07b638cc61ecb593968394eeedf2144f5b
cgarn3 <clintman@bellsouth.net>
2015-09-01 19:09:50 -0400

    Added updated Person5.java and .gitignore

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -1,57 +1,60 @@
 package edu.gatech.oad.antlab.person;
-
+    
 /**
- *  A simple class for person 5
- *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
- *  @version 1.1
- */
+* * A simple class for person 5
+* * returns their name and a
+* * modified string
+* *
+* * * @author Bob
+* * @version 1.1
+* */
+            
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person5(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 5 put your implementation here 
-            if (input.length() > 2) {
-                return(input.substring(2, input.length()) + input.substring(0, 2));
-            } else {
-                return (input);
-            }
-        } 
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	    return name + calc(input);
-	}
-
-        public static void main(String[] args) {
-            Person5 p5 = new Person5("Clint");
-            System.out.println(p5.toString("gtg123b"));
+    /** Holds the persons real name */
+    private String name;
+    /**
+                     
+     * * The constructor, takes in the persons
+    * * name
+    * * @param pname the person's real name
+    * */
+    public Person5(String pname) {
+        name = pname;
+    }
+                    
+    /**
+    * * This method should take the string
+    * * input and return its characters rotated
+    * * 2 positions.
+    * * given "gtg123b" it should return
+    * * "g123bgt".
+    * *
+    * * @param input the string to be modified
+    * * @return the modified string
+    * */
+    private String calc(String input) {
+        //Person 5 put your implementation here
+        if (input.length() > 2) {
+            return(input.substring(2, input.length()) + input.substring(0, 2));
+        } else {
+            return (input);
         }
+    }
+                      
+    /**
+    * Return a string rep of this object
+    * that varies with an input string
+    *
+    * @param input the varying string
+    * @return the string representing the
+    * object
+    */
+    public String toString(String input) {
+        return name + calc(input);
+    }
+        
+    public static void main(String[] args) {
+        Person5 p5 = new Person5("Clint");
+        System.out.println(p5.toString("gtg123b"));
+    }
 }
