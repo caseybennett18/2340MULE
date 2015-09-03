40928415e159588f6f608139ddcc2b9654b8c20c
cgarn3 <clintman@bellsouth.net>
2015-09-03 12:22:57 -0400

    Updated p5 name in AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("Leia Glezer");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Clint");
+                  Person5 p5 = new Person5("Clint Garn");
                   toPrint += p5.toString("cgarn3");
 		  
         System.out.println(toPrint);
