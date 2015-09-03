3f1d1b94401ac28ec9a120a480832ef7bd44e269
charlesrmarcus <charlesrmarcus@gmail.com>
2015-09-03 12:44:08 -0400

    Charlie Marcus Person3.java changed

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+      String output = "";
+	  for (int i = input.length() - 1; i >= 0; i++) {
+          output = output + input.charAt(i);
+      }
+      return output;
 	}
 }
