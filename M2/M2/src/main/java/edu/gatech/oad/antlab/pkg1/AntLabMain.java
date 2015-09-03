938d1d3b4ed80abb929bc16e3aa074918ee75a06
charlesrmarcus <charlesrmarcus@gmail.com>
2015-09-03 12:49:05 -0400

    Charlie altered AntLabMain.java p3 print statement

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  toPrint += p3.toString("Charles Marcus");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
