2b25d821b6c3db2c0a967fec6cf4f94f4cda0203
cpursley22 <chelseapursley@gmail.com>
2015-09-23 22:36:37 -0400

    javadocs + spacing

--- src/models/Race.java
+++ src/models/Race.java
@@ -1,16 +1,35 @@
 package models;
 
+
+/**
+ * Holds information about the race
+ *
+ */
 public class Race {
     
+    /**
+	 * id of player who is the Race
+	 */
 	private int playerId;
 	
+	/**
+	 * Creates a new race associated to a player through their playerId
+	 * @param playerId
+	 */
 	public Race(int playerId) {
 		this.playerId = playerId;
 	}
 	
+	/**
+	 * Applies race modifier bonus to that player
+	 */
 	public void applyRaceModifier() {
 	}
 
+	/**
+	 * @return string "DefaultRace"
+	 * Applies race modifier bonus to that player
+	 */
 	public String toString() {
 		return "DefaultRace";
 	}
