ebe1989c32a22f9389a4b63cb3f036effe9c3a94
charlesrmarcus <charlesrmarcus@gmail.com>
2015-09-23 21:11:45 -0400

    refactor MapScreenController.java

---
+++ src/MapScreenController.java
@@ -0,0 +1,28 @@
+ import java.net.URL;
+ import java.util.ResourceBundle;
+ import javafx.event.ActionEvent;
+ import javafx.fxml.FXML;
+ import javafx.fxml.Initializable;
+ import javafx.scene.control.Label;
+ import javafx.scene.*;
+ import javafx.scene.control.MenuItem;
+ import javafx.stage.Stage;
+ import javafx.fxml.FXMLLoader;
+ import javafx.scene.control.Button;
+ import java.io.IOException;
+
+ 
+ public class MapScreenController implements Initializable {
+     
+    
+    
+    public MapScreenController() {
+    }
+    
+    
+    @FXML
+    public void initialize(URL url, ResourceBundle rb ) {
+    }
+
+}   
+  
