f717749e51da4804dd94bd9e2710505076e8e0f3
cgarn3 <clintman@bellsouth.net>
2015-10-06 18:22:52 -0400

    Fixed timer. Once you gamble, it goes to the next players turn. All hail Trevor!

--- src/MapScreenController.java
+++ src/MapScreenController.java
@@ -53,12 +53,16 @@
 
      private final static int TILE_WIDTH = 78;
      private final static int TILE_HEIGHT = 120;
+
+     private static MapScreenController instance;
      private Player[] players;
      private Round round;
      private Button clickedButton;
      protected boolean timeRanOut = false;
+     final NextButtonPressedHandler nextTurnHandler = new NextButtonPressedHandler();
     
     public MapScreenController() {
+        instance = this;
         this.players = MuleUI.getInstance().getPlayerArray();
         MuleUI.getInstance().getPlayerOrder(players);
         this.round = new Round(players);
@@ -66,6 +70,13 @@
         timer = new Timer();
     }
 
+     public static MapScreenController getInstance() {
+         return instance;
+     }
+
+     public void nextTurn() {
+         nextTurnHandler.nextTurn();
+     }
 
      @FXML
      public void handleTown() throws Exception {
@@ -82,10 +93,6 @@
             currentPlayer.setHasPicked(true);
             currentPlayer.incrementNumProperties();
         }
-
-
-
-
     }
 
      public VBox addPlayerAttributes() {
@@ -121,7 +128,7 @@
          tf.getChildren().add(vboxx);
      }
 
-    private void generateScreen() {
+     private void generateScreen() {
         Group root = new Group();
 
         VBox vbox = new VBox(); //has another vbox with a label and a button
@@ -130,15 +137,14 @@
         root.getChildren().add(vbox);//root has one vbox
 
         anchorpane.getChildren().add(root);//has a group
-    }
-
+     }
 
      private class NextButtonPressedHandler implements EventHandler<MouseEvent> {
-
 
          String labelText;
 
         public void nextTurn() {
+            timer.setTime(31);
             if (!landSelectionPhaseOver) {
                 //going to next round
                 if (round.turnPhase == players.length) {
@@ -203,8 +209,6 @@
 
             }
 
-
-
             TextFlow root = (TextFlow) borderpane.getChildren().get(2);
             VBox vbox2 = (VBox) root.getChildren().get(0);
 
@@ -231,11 +235,11 @@
          public void handle(MouseEvent me) {
              clickedButton = (Button) me.getSource();
              if (!(clickedButton.isDisabled())) {
-                     nextTurn();
+                 nextTurn();
              }
 
              if (timeRanOut) {
-                 timer.setTime(10);
+                 timer.setTime(31);
                  timeRanOut = false;
              }
          }
